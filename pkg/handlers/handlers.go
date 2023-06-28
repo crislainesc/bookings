8cd6863a7c491c92cb6cce6326bde712c3a3a2e0
Crislâine Santos <criis.santos@outlook.com>
2023-06-28 11:36:49 -0300

    feat: add handlers and main program

---
+++ pkg/handlers/handlers.go
@@ -0,0 +1,17 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/crislainesc/bookings/pkg/render"
+)
+
+// Home is the handler for the home page
+func Home(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "home.page.tmpl.html")
+}
+
+// About is the handler for the about page
+func About(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "about.page.tmpl.html")
+}
