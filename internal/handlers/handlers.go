1ad3a6ea506fc479a2758b7f6731a3c88f66d34e
Crislâine Santos <criis.santos@outlook.com>
2023-07-04 11:07:11 -0300

    feat: add insert reservation function

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/crislainesc/bookings/internal/config"
 	"github.com/crislainesc/bookings/internal/driver"
@@ -68,11 +70,32 @@ func (repository *Repository) PostReservation(w http.ResponseWriter, r *http.Req
 		return
 	}
 
+	sd := r.Form.Get("start_date")
+	ed := r.Form.Get("end_date")
+	layout := "2006-01-02"
+
+	startDate, err := time.Parse(layout, sd)
+	if err != nil {
+		helpers.ServerError(w, err)
+	}
+	endDate, err := time.Parse(layout, ed)
+	if err != nil {
+		helpers.ServerError(w, err)
+	}
+
+	roomID, err := strconv.Atoi(r.Form.Get("room_id"))
+	if err != nil {
+		helpers.ServerError(w, err)
+	}
+
 	reservation := models.Reservation{
 		FirstName: r.Form.Get("first_name"),
 		LastName:  r.Form.Get("last_name"),
 		Email:     r.Form.Get("email"),
 		Phone:     r.Form.Get("phone"),
+		StartDate: startDate,
+		EndDate:   endDate,
+		RoomID:    roomID,
 	}
 
 	form := forms.New(r.PostForm)
@@ -89,6 +112,11 @@ func (repository *Repository) PostReservation(w http.ResponseWriter, r *http.Req
 			Data: data,
 		})
 		return
+	}
+
+	err = repository.DB.InsertReservation(reservation)
+	if err != nil {
+		helpers.ServerError(w, err)
 	}
 
 	repository.App.Session.Put(r.Context(), "reservation", reservation)
--- internal/repository/dbrepo/postgres.go
+++ internal/repository/dbrepo/postgres.go
@@ -1,5 +1,39 @@
 package dbrepo
 
-func (m *postgresDBRepo) AllUsers() bool {
+import (
+	"context"
+	"time"
+
+	"github.com/crislainesc/bookings/internal/models"
+)
+
+func (repository *postgresDBRepo) AllUsers() bool {
 	return true
+}
+
+func (repository *postgresDBRepo) InsertReservation(res models.Reservation) error {
+	context, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+
+	defer cancel()
+
+	query := `INSERT INTO 	reservations (first_name, last_name, email, phone, start_date, end_date,
+		room_id, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)`
+
+	_, err := repository.DB.ExecContext(context, query,
+		res.FirstName,
+		res.LastName,
+		res.Email,
+		res.Phone,
+		res.StartDate,
+		res.EndDate,
+		res.RoomID,
+		time.Now(),
+		time.Now(),
+	)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
