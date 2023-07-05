eb525bcb371252a9f76682f8dd2a1af3d96892ed
Crislâine Santos <criis.santos@outlook.com>
2023-07-04 21:10:17 -0300

    feat: add seeds to rooms and restrictions tables

---
+++ migrations/20230705000653_seed_rooms_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.rooms (room_name,created_at,updated_at) VALUES
+	 ('General''s Quaters','2023-07-04 00:00:00.000','2023-07-04 00:00:00.000'),
+	 ('Marjor''s Suite','2023-07-04 00:00:00.000','2023-07-04 00:00:00.000');
