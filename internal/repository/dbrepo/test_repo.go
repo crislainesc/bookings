9e830144a09c1dd4e1af3bbfa881f56a9939c955
Crislâine Santos <criis.santos@outlook.com>
2023-07-06 15:38:51 -0300

    test: add functions to edit reservation

--- internal/repository/dbrepo/test_repo.go
+++ internal/repository/dbrepo/test_repo.go
@@ -134,3 +134,15 @@ func (m *testDBRepo) GetReservationByID(id int) (models.Reservation, error) {
 	var res models.Reservation
 	return res, nil
 }
+
+func (m *testDBRepo) UpdateReservation(reservation models.Reservation) error {
+	return nil
+}
+
+func (m *testDBRepo) DeleteReservation(id int) error {
+	return nil
+}
+
+func (m *testDBRepo) UpdateProcessedForReservation(id, processed int) error {
+	return nil
+}
